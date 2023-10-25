99e083d627e85b366d0c69970863ad95fb204e79
Kevin Ribeiro Vieira <132036022+kevin-ribeiro@users.noreply.github.com>
2023-10-25 15:45:19 -0300

    Comentarios js questoes e separacao dos botoes embaixo das questoes

--- questoes/script.js
+++ questoes/script.js
@@ -63,26 +63,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 // --MODAL DE VERIFICAÇÃO DE RESPOSTA--
-// Função para exibir a modal com a mensagem
-function exibirModal(mensagem) {
-  const modal = document.getElementById("myModal");
-  const modalMessage = document.getElementById("modalMessage");
-  modalMessage.innerHTML = mensagem.replace(/\\n/g, '<br>');
-  modal.style.display = "block";
+function exibirModal(mensagem) { // Função para exibir a modal com a mensagem
+  const modal = document.getElementById("myModal"); // Obtém o elemento modal pelo ID
+  const modalMessage = document.getElementById("modalMessage"); // Obtém o elemento da mensagem modal pelo ID
+  modalMessage.innerHTML = mensagem.replace(/\\n/g, '<br>'); // Substitue os caracteres de nova linha na mensagem por tags <br> para formatação HTML correta
+  modal.style.display = "block"; // Exibe a modal alterando o estilo de exibição para "block"
 
-  // Fechar a modal quando clicar no botão 'X'
+  // Fechar a modal quando clicar no botão definido em HTML
   const modalClose = document.getElementById("modalClose");
-  modalClose.onclick = function() {
-    modal.style.display = "none";
+  modalClose.onclick = function() { // Define um manipulador de eventos onclick para o botão de fechamento da modal
+    modal.style.display = "none"; // Oculta a modal alterando o estilo de exibição para "none"
   }
 }
 
+// Função para validar a resposta selecionada pelo usuário
 function validarResposta() {
-  const alternativas = document.getElementsByName("alternativa");
+  const alternativas = document.getElementsByName("alternativa"); // Obtém todos os elementos de input do tipo radio com o nome "alternativa"
   let respostaSelecionada = "";
 
+  // Percorre todos os elementos de input do tipo radio
   for (let i = 0; i < alternativas.length; i++) {
-    if (alternativas[i].checked) {
+    // Se o input do tipo radio estiver marcado, esse bloco de comando armazena seu valor em respostaSelecionada e interrompa o loop
+    if (alternativas[i].checked) { 
       respostaSelecionada = alternativas[i].value;
       break;
     }
@@ -91,25 +93,29 @@ function validarResposta() {
   // Obtenha o número da questão do título da página
   let numeroQuestao = document.querySelector('h1').innerText.split('/')[0].split(' ')[1];
 
+  // Se nenhuma alternativa foi selecionada, exibe uma mensagem na modal
   if (respostaSelecionada === "") {
     exibirModal("Por favor, selecione uma alternativa.");
-  } else {
+  } else { // Se uma alternativa foi selecionada, obtém a resposta correta do atributo 'cor' do input do tipo radio marcado
     const respostaCorreta = document.querySelector('input[name="alternativa"]:checked').getAttribute('cor');
+    // Se a resposta selecionada for igual à resposta correta, exibe uma mensagem de sucesso na modal
     if (respostaSelecionada === respostaCorreta) {
       exibirModal("Resposta correta!\nAlternativa selecionada: " + respostaSelecionada);
+    // Se a resposta selecionada for diferente da resposta correta, exibe uma mensagem de erro na modal
     } else {
       exibirModal("Resposta incorreta.\nAlternativa selecionada: " + respostaSelecionada + "\nAlternativa correta: " + respostaCorreta);
     }
 
-    // Desabilitar todos os inputs radio
+    // Desabilita todos os inputs radio após a verificação da resposta
     for (let i = 0; i < alternativas.length; i++) {
       alternativas[i].disabled = true;
     }
-    // Armazene um valor no localStorage para indicar que o usuário já clicou no botão
+    // Armazena um valor no localStorage para indicar que o usuário já clicou no botão de verificação da resposta
   localStorage.setItem('verificado' + numeroQuestao, 'true');
   }
 }
 
+// Função para salvar a resposta selecionada
 function salvarResposta() {
   // Obtenha a resposta selecionada
   const alternativas = document.getElementsByName("alternativa");
